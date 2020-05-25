a2defc4f5d196d7ce146bf29d5e62c9ec19b6a30
Mategrze <mgrzelak1997@wp.pl>
2020-05-25 17:54:11 +0200

    set parent component and add jokes page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,20 +9,28 @@ import { RouteGuardService } from './service/route-guard.service';
 import { UpdateTodoComponent } from './update-todo/update-todo.component';
 import { AboutWebsiteComponent } from './about-website/about-website.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { LoggedContentComponent } from './logged-content/logged-content.component';
+import { JokesComponent } from './jokes/jokes.component';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent, data:{animation: "LoginPage"} },
-  { path: 'about', component: AboutWebsiteComponent, data:{animation: "AboutPage"} },
-  { path: 'registration', component: RegistrationComponent, data:{animation: "RegistrationPage"} },
-  { path: '', component: WelcomeComponent, canActivate: [RouteGuardService]},
-  { path: 'todos', component: ListTodosComponent, canActivate: [RouteGuardService] },
-  { path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService] },
-  { path: 'todos/update/:id', component: UpdateTodoComponent, canActivate: [RouteGuardService] },
-  
+  { path: 'login', component: LoginComponent, data: { animation: "LoginPage" } },
+  { path: 'about', component: AboutWebsiteComponent, data: { animation: "AboutPage" } },
+  { path: 'registration', component: RegistrationComponent, data: { animation: "RegistrationPage" } },
+  {
+    path: '', component: LoggedContentComponent, data: { animation: "ContentPage" },
+    children: [
+      { path: '', redirectTo: 'todos', pathMatch: 'full'},
+      { path: 'todos', component: ListTodosComponent, data: { animation: "TodosPage" } },
+      { path: 'logout', component: LogoutComponent, data: { animation: "LogoutPage" } },
+      { path: 'todos/update/:id', component: UpdateTodoComponent, },
+      { path: 'welcome', component: WelcomeComponent, data: { animation: "WelcomePage" }},
+      { path: 'jokes', component: JokesComponent, data: { animation: "JokesPage" }},
+    ]
+  },
   { path: '**', component: ErrorComponent }
 ];
-
+// canActivate: [RouteGuardService] 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
