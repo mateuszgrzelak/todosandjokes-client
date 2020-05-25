1c72f55ac27e986c9e38c2addf3f385a930e72eb
Mategrze <mgrzelak1997@wp.pl>
2020-05-25 17:52:44 +0200

    Add animation after log in

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -3,7 +3,7 @@ import { trigger, transition, style, query, animateChild, group, animate } from 
 export const slideInAnimation =
   trigger('routeAnimations', [
     transition('LoginPage => AboutPage', [
-      style({ position: 'relative'}),
+      style({ position: 'relative' }),
       query(':enter, :leave', [
         style({
           position: 'absolute',
@@ -13,18 +13,18 @@ export const slideInAnimation =
         })
       ]),
       query(':enter', [
-        style({ left: '-100%'})
+        style({ left: '-100%' })
       ]),
       query(':leave', [
-        style({ position: 'absolute'})
+        style({ position: 'absolute' })
       ]),
       query(':leave', animateChild()),
       group([
         query(':leave', [
-          animate('1000ms ease-out', style({ left: '100%'}))
+          animate('1000ms ease-out', style({ left: '100%' }))
         ]),
         query(':enter', [
-          animate('1000ms ease-out', style({ left: '0%'}))
+          animate('1000ms ease-out', style({ left: '0%' }))
         ])
       ]),
       query(':enter', animateChild()),
@@ -40,15 +40,15 @@ export const slideInAnimation =
         })
       ]),
       query(':enter', [
-        style({ left: '100%'})
+        style({ left: '100%' })
       ]),
       query(':leave', animateChild()),
       group([
         query(':leave', [
-          animate('1000ms ease-out', style({ left: '-100%'}))
+          animate('1000ms ease-out', style({ left: '-100%' }))
         ]),
         query(':enter', [
-          animate('1000ms ease-out', style({ left: '0%'}))
+          animate('1000ms ease-out', style({ left: '0%' }))
         ])
       ]),
       query(':enter', animateChild()),
@@ -65,15 +65,15 @@ export const slideInAnimation =
         })
       ]),
       query(':enter', [
-        style({ transform: 'translateY(-100%)'})
+        style({ transform: 'translateY(-100%)' })
       ]),
       query(':leave', animateChild()),
       group([
         query(':leave', [
-          animate('1000ms ease-out', style({ transform: 'translateY(100%)'}))
+          animate('1000ms ease-out', style({ transform: 'translateY(100%)' }))
         ]),
         query(':enter', [
-          animate('1000ms ease-out', style({ transform: 'translateY(0%)'}))
+          animate('1000ms ease-out', style({ transform: 'translateY(0%)' }))
         ])
       ]),
       query(':enter', animateChild()),
@@ -90,18 +90,72 @@ export const slideInAnimation =
         })
       ]),
       query(':enter', [
-        style({ transform: 'translateY(100%)'})
+        style({ transform: 'translateY(100%)' })
       ]),
       query(':leave', animateChild()),
       group([
         query(':leave', [
-          animate('1000ms ease-out', style({ transform: 'translateY(-100%)'}))
+          animate('1000ms ease-out', style({ transform: 'translateY(-100%)' }))
         ]),
         query(':enter', [
-          animate('1000ms ease-out', style({ transform: 'translateY(0%)'}))
+          animate('1000ms ease-out', style({ transform: 'translateY(0%)' }))
         ])
       ]),
       query(':enter', animateChild()),
     ]),
-    
+    transition('ContentPage <=> LoginPage', [
+      style({}),
+      query(':enter, :leave', [
+        style({
+          'z-index': 10,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+        })
+      ]),
+      query(':enter', [
+        style({ opacity: 0 })
+      ]),
+      query('router-outlet ~ *', [style({}), animate(1, style({}))], { optional: true }), // must be added to animate router-outlet
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('1000ms ease-out', style({ opacity: 0 }))
+        ]),
+        query(':enter', [
+          animate('1000ms ease-out', style({ opacity: 1 }))
+        ])
+      ]),
+      query(':enter', animateChild()),
+    ]),
+  ]);
+
+export const loggedContent =
+  trigger('routeAnimations', [
+    transition('TodosPage <=> JokesPage', [
+      style({}),
+      query(':enter, :leave', [
+        style({
+          height: '100%',
+          'z-index': 10,
+          position: 'absolute',
+          width: '100%'
+        })
+      ]),
+      query(':enter', [
+        style({ opacity: 0 })
+      ]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('500ms ease-out', style({ opacity: 0 }))
+        ]),
+        query(':enter', [
+          animate('500ms ease-out', style({ opacity: 1 }))
+        ])
+      ]),
+      query(':enter', animateChild()),
+    ]),
   ]);
