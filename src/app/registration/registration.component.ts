6f9fe69eaa9d9713ae4ea4c102872c5d7997ac99
Mategrze <mgrzelak1997@wp.pl>
2020-06-02 23:46:28 +0200

    Add form validation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { MustMatch } from '../validation/must-match';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  userAccountForm: FormGroup;
+  // username: string=''; //minLength = 4
+  // password: string=''; //minLength = 5
+  // confirmPassword: string=''; //minLength = 5, equal to password
+  // email: string=''; // proper syntax
+
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.userAccountForm = this.formBuilder.group({
+      'username': new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'password': new FormControl('', [
+        Validators.required,
+        Validators.minLength(5)
+      ]),
+      'confirmPassword': new FormControl('', [
+        Validators.required,
+        Validators.minLength(5)
+      ]),
+      'email': new FormControl('', [
+        Validators.required,
+        Validators.email
+      ])
+    },{
+      validator: MustMatch('password', 'confirmPassword')
+    });
+  }
+
+  get f() { return this.userAccountForm.controls; }
+
+  register(){
+    console.log('registration');
   }
 
 }
