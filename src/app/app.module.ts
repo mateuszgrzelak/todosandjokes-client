4576b5b8dde57a5a80027ebfee997ddae37fcba9
Mategrze <mgrzelak1997@wp.pl>
2020-06-02 23:46:03 +0200

    Add ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -47,7 +47,8 @@ import { LoaderInterceptor } from './service/http/loader-interceptor.service';
     FormsModule,
     HttpClientModule,
     ParticlesModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    ReactiveFormsModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: HttpIntercepterBasicAuthService, multi:true},
