d64dafca4349173d104ab3826aa03610c156f371
Mategrze <mgrzelak1997@wp.pl>
2020-06-05 20:02:38 +0200

    Create window about registration status

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { MustMatch } from '../validation/must-match';
+import { AuthenticationService } from '../service/security/authentication.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ResultDialogComponent } from './result-dialog/result-dialog.component';
+import { Router } from '@angular/router';
+
+export class User{
+  constructor(public username:string, public password: string, public email: string){}
+}
 
 @Component({
   selector: 'app-registration',
@@ -11,9 +19,11 @@ export class RegistrationComponent implements OnInit {
 
   registerForm: FormGroup;
   passwordSize = 5;
+  errorMessage=false;
 
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private authentication: AuthenticationService, 
+    public matDialog: MatDialog, private router:Router) { }
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
@@ -39,15 +49,32 @@ export class RegistrationComponent implements OnInit {
   get f() { return this.registerForm.controls; }
 
   register(){
-    console.log(this.registerForm.controls.username.value);
-    console.log(this.registerForm.controls.password.value);
-    
+
     if(this.registerForm.invalid){
       return;
     }
+    
+    this.authentication.createAccount(new User(this.f.username.value, this.f.password.value, this.f.email.value)).subscribe(
+        data => {
+          const dialogRef = this.matDialog.open(ResultDialogComponent, {
+            panelClass: 'registration-result-dialog',
+            data: this.f.email.value
+          });
+          dialogRef.afterClosed().subscribe(result => {
+            this.router.navigate(['login']);
+          });
+        },
+        error =>{
+          this.errorMessage=true;
+          this.f.password.setValue('');
+          this.f.confirmPassword.setValue(''); 
+        }
+    );
 
-    console.log('registration');
+  }
 
+  closeErrorMessage(){
+    this.errorMessage=false;
   }
 
 }
