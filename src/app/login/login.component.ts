6e1808ed88a70ccd6511388bb46377dd3b33becb
Mategrze <mgrzelak1997@wp.pl>
2020-06-06 18:11:02 +0200

    Configure invalid login or password info and create identifaction process

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BasicAuthenticationService } from '../service/basic-authentication.service';
+import { AuthenticationService } from '../service/security/authentication.service';
+
+export class User{
+  constructor(public username:string, public password:string){}
+}
 
 @Component({
   selector: 'app-login',
@@ -11,24 +16,44 @@ export class LoginComponent implements OnInit {
   username = '';
   password = '';
   errorMessage = 'Invalid Credentials';
-  invalidLogin: boolean;
+  invalidUsername=false;
+  invalidPassword=false;
+  loginFailed=false;
 
   constructor(private router: Router,
-    private basicAuthenticationService: BasicAuthenticationService) { }
+    private authentication: AuthenticationService) { }
 
   ngOnInit(): void {}
 
   handleBasicAuthLogin() {
-    this.basicAuthenticationService.executeBasicAuthenticationService(this.username, this.password).subscribe(
+    if(this.username===''){
+      this.invalidUsername = true;
+    }else{
+      this.invalidUsername = false;
+    }
+    if(this.password===''){
+      this.invalidPassword = true;
+    }else{
+      this.invalidPassword = false;
+    }
+    if(this.invalidUsername || this.invalidPassword){
+      console.log('nieprawidlowe');
+      return;
+    }
+    this.authentication.identificate(new User(this.username, this.password)).subscribe(
       data => {
-        this.router.navigate(['todos'])
-        this.invalidLogin = false;
+        console.log(data);
+        // this.router.navigate(['todos'])
       },
       error => {
-        this.invalidLogin = true;
+        this.loginFailed=true;
       }
     )
 
+  }
+
+  closeErrorMessage(){
+    this.loginFailed=false;
   }
 
 }
